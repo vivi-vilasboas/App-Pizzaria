689dcecfcdb79b547712201074a372c90af57505
unknown <viviane_boas@yahoo.com.br>
2022-09-26 22:18:03 -0300

    criando models em schema.prisma

---
+++ backend/prisma/migrations/20220927004939_create_table_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passaword" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,6 @@ import { Router, Request, Response } from 'express';
 const router = Router()
 
 router.get('/teste', (req: Request, res: Response) => {
-    //throw new Error('Erro ao fazer essa requisição')
     return res.json({ ok: true })
 })
 
