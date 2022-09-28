dd1f57bcadad0b7baa76f49c6ef27b17074c484a
unknown <viviane_boas@yahoo.com.br>
2022-09-28 11:42:55 -0300

    criptografando senha

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import prismaClient from '../../prisma';
+import { hash } from 'bcryptjs'
+
 interface UserRequest {
     name: string;
     email: string;
@@ -7,7 +10,34 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({name, email, password}: UserRequest) {
-        return { name: name, email: email, password: password }
+        if(!email) {
+            throw new Error("Email incorrect")
+        }
+        
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if(userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+
+        const passawordHash = await hash(password, 8)
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                passaword: passawordHash
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true
+            }
+        })
+        return user
     }
 }
 
