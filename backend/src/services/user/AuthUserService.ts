601c88ab5e45418cf95ff7d603704ed88e5e4e4e
unknown <viviane_boas@yahoo.com.br>
2022-09-28 17:25:31 -0300

    gerando token

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import {compare} from "bcryptjs"
+import { sign } from 'jsonwebtoken'
 
 interface AuthRequest {
     email: string,
@@ -23,9 +24,24 @@ class AuthUserService {
         if(!passwordMatch) {
             throw new Error("Password incorrect")
         }
-
-        return { ok: true}
-
+        
+        const token = sign(
+            {
+            name: user.name,
+            email: user.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        } 
     }
 }
 
