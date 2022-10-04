1d1e69702698c39f3d9e38b82139e73ff6a081d5
unknown <viviane_boas@yahoo.com.br>
2022-10-03 22:39:26 -0300

    add controllers e services para cadastrar categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,9 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
@@ -11,5 +14,8 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
---
+++ backend/src/services/category/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import prismaClient from "../../prisma";
+
+interface CateroryResquest {
+    name: string
+}
+
+class CreateCategoryService {
+    async execute({ name }: CateroryResquest) {
+        if(name === '') {
+            throw new Error('Name invalid')
+        }
+
+        const category = await prismaClient.category.create({
+            data: {
+                name: name
+            },
+            select: {
+                id: true,
+                name: true
+            }
+        })
+
+        return category
+    }
+}
+
+export { CreateCategoryService }
