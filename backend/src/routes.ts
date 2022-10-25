33f0bf1fc9ae7983d2db48bf0392b9b4166fd57c
unknown <viviane_boas@yahoo.com.br>
2022-10-24 23:10:47 -0300

    listando todos os produtos de uma mesma categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -26,6 +27,7 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 
 export { router };
